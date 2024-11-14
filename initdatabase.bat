2d768320326d7cfd4b201fe5e443e06ba1276c8d
unknown <gab.pin.fer@hotmail.com>
2024-11-13 21:20:32 -0300

    init:bat de criação do banco

---
+++ initdatabase.bat
@@ -0,0 +1,17 @@
+@echo off
+:: Armazenar o diretório atual (onde o script .bat está sendo executado)
+set original_dir=%cd%
+
+:: Exibir o diretório armazenado (opcional, apenas para verificação) echo Diretório original: %original_dir%
+
+:: Mudar para o diretório do MySQL
+cd C:\xampp\mysql\bin
+
+:: Rodar o MySQL e importar o banco.sql (considerando que banco.sql está no mesmo diretório que o package.json)
+mysql -u root -p < "%original_dir%\banco.sql"
+
+:: Voltar para o diretório original
+cd /d %original_dir%
+
+:: Exibir uma mensagem de sucesso echo Retornado ao diretório original: %original_dir%
+echo Tabelas criada com sucesso
